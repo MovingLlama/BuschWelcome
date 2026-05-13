5c19b85258d16d79086071b7ac3885dbe0f6b7b4
MovingLlama <stefan@seyerl.eu>
2026-05-13 20:16:18 +0200

    fix: add missing constants for portal.py

--- custom_components/busch_welcome/const.py
+++ custom_components/busch_welcome/const.py
@@ -13,3 +13,13 @@ DEFAULT_PORT = 80
 UPDATE_INTERVAL = 30  # seconds for polling if SIP/Webhooks are not used
 
 PLATFORMS = ["binary_sensor", "camera", "switch", "sensor"]
+
+GEO_URL = "https://geo.mybuildings.abb.com"
+DEFAULT_PORTAL_URL = "https://api.eu.mybuildings.abb.com"
+
+CLIENT_TYPE = "com.abb.ispf.client.globalip.app.abb.android"
+GATEWAY_CLIENT_TYPE = "com.abb.ispf.client.welcome.gateway"
+
+EVENT_TYPE_DISCOVERY = "com.abb.ispf.event.discovery"
+EVENT_TYPE_CONNECT = "com.abb.ispf.event.welcome.connect"
+EVENT_TYPE_ACL_UPDATE = "com.abb.ispf.event.welcome.acl-update"
